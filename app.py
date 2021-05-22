440218aceb1e72c9d99f3ee7419afada486b14e3
Ali Haider <alihaider1050@gmail.com>
2021-05-23 03:35:04 +0500

    Add fuzzy search

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 from fm_index import FMIndex
 from util import save_pickle, load_pickle, load_files, get_file_name_via_index
 import time
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
     return 'Fuzzy DNA Search API'
-
 
 @app.route('/search', methods=['GET', 'POST'])
 def index():
@@ -51,44 +51,38 @@ def index():
         datafile = 'test_data'
 
         print('encode text...')
+
+        start = time.time()
         bw, sa = fmi.encode(T)
+        stop = time.time()
+        
+        print('suffix array time:', stop-start)
+        
+        start = time.time()
         ranks, ch_count = fmi.rank_bwt(bw)
+        stop = time.time()
+        
+        print('ranking time:', stop-start)
+        
         save_pickle({'bwt': bw, 'sa': sa, 'text_len': len(T), 'ch_count': ch_count}, 'index.dict')
         fmi.ch_count = ch_count
+        
         print('encode done!')
 
         start = time.time()
-        match = fmi.search(substring)
-        stop = time.time()
-        print(stop-start)
-        # print('Results of search("{}")'.format(substring))
-        # print('match', match)
-        return {'match' : match}
-        # data.save(os.path.join('data/', datafile))
-        # data = file.read()
-        # print('starting building Index')
-        # start = time.time()
-        # idx = fmindex.index(data)
-        # stop = time.time()
-        # print('FM-Index built in:', stop-start, 'seconds')
-        # fmindex.save(save_idx, idx)
-        # print('FM-Index saved!')
-        # print()
-        # print('Fuzzy DNA matching in progress. . .')
+
+        results = {}
+        for i in range(len(substring), int(len(substring)*0.79), -1):
+            s = substring[:i]
+            perc = int((len(s) / len(substring)) * 100)
+            match = fmi.search(s)
+            results[perc] = match
         
-        # d = {}
-        # start = time.time()
-        # for i in range(len(substring), int(len(substring)*0.65), -1):
-        #     s = substring[:i]
-        #     score = int((len(s)/len(substring)) * 100)
-        #     ans = idx.search(s)
-        #     d[score] = (s,ans)
-        # stop = time.time()
-        # print()
-        # print('Fuzzy Matching done in', stop-start, 'seconds')
-        # print()
-        # print(d)
-        # return d
+        stop = time.time()
+
+        print('searching time:', stop-start)
+        return results
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()

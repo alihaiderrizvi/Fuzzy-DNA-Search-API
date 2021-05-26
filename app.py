ed9b1d62939f13fbe2a24cebef0d51ab37a78934
Ali Haider Rizvi <45711004+alihaiderrizvi@users.noreply.github.com>
2021-05-27 02:48:36 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def index():
         results = fuzzy_search(substring, fmi)
         
         stop = time.time()
-        results['search time'] = str(stop-start)
+        results[-1] = stop-start
         print('searching time:', stop-start)
         print('results:', results)
         return jsonify(results)
@@ -112,10 +112,10 @@ def preloaded():
         start = time.time()
         results = fuzzy_search(substring, fmi)
         stop = time.time()
-        results['search time'] = stop-start
+        results[-1] = stop-start
         print('searching time:', stop-start)
-        print('results:', results)
-        return results
+        # print('results:', results)
+        return jsonify(results)
 
 if __name__ == '__main__':
     app.debug = True
